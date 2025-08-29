f7bf4d1fa99dabc2f6f4aff71752779ecd71c9b5
Aefrir <d.nguyen@rcnheliomare.nl>
2025-08-29 14:49:29 +0200

    asd

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+	
+?>
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta name="robots" content="index,follow">
+		<title>Portfolio</title>
+		<link rel="stylesheet" href="css/stylesheet.css">
+		<script src="https://kit.fontawesome.com/a0a5e50891.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<h1>Portfolio</h1>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+			<div class="socials">
+				<a href="#"><i class="fa-brands fa-twitter"></i></a>
+				<a href="#"><i class="fa-brands fa-facebook"></i></a>
+				<a href="#"><i class="fa-brands fa-google"></i></a>
+			</div>
+		</header>
+		<main></main>
+		<footer></footer>
+	</body>
+</html>
