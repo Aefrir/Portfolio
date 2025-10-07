ff37d041038607c314aa8d232c9ccb3e14cb1313
Aefrir <daanphin@gmail.com>
2025-10-07 14:46:35 +0200

    asd

--- index.php
+++ index.php
@@ -42,8 +42,18 @@
 				<h2>About Me</h2>
 				<div class="about-me-content">
 					<article>
-						<p>I am currently a student enrolled at REA College studying Webdevelopment. So far I've finished my HTML, CSS, SEO and my introductory JavaScript course. You can see the projects i've worked on in the project section.</p>
+						<h3>Introduction</h3>
+						<p>During my studies at Inholland University in Alkmaar, I discovered my passion for programming. I'm currently continuing my journey at REA College, specializing in web development and working with various programming languages. I enjoy building clean, functional, and creative web experiences that bring ideas to life.</p>
 					</article>
+					<article>
+						<h3>Goals</h3>
+						<p>I'm eager to apply my knowledge in a professional environment and am looking for an internship as a web developer where I can continue to grow, learn from others, and refine my skills. I'm motivated by the challenge of turning ideas into interactive, user-friendly solutions.</p>
+					</article>
+					<article>
+						<h3>Hobbies</h3>
+						<p>Outside of coding, I'm a creative at heart. I enjoy watching anime, reading comics, and creating digital art, hobbies that inspire my sense of design and storytelling. I also play and analyze games, often diving into how they work behind the scenes. These interests fuel my curiosity and help me approach web development from both a technical and artistic perspective.</p>
+					</article>
+					
 				</div>
 
 			</section>
@@ -51,24 +61,28 @@
 				<h2>Projects</h2>
 				<div class="project-content">
 					<article>
-						<h4>HTML</h4>
+						<h3>HTML</h3>
 						<p>In HTML I've made a <a href="#" class="webpage-link">webpage</a> containing a table with a detailed description of all the metatags, elements, attributes that I've learned during my HTML course.</p>
+						<p>Eindopdracht + Afbeeldingen opdracht 5 + 6</p>
 					</article>
 					<article>
-						<h4>CSS</h4>
+						<h3>CSS</h3>
 						<p>In CSS I've created a medical <a href="#" class="webpage-link">webpage</a> of a Webshop designed in Figma.</p>
+						<p>Flexbox 4, Grid 7, Animaties key + 6, Mediaqueries 4 + 6, Scrollen 6 + 7 & Variabelen 5</p>
 					</article>
 					<article>
-						<h4>SEO</h4>
+						<h3>SEO</h3>
 						<p>SEO is part of a webpage that you don't see as a customer hence why it is hard to show an actual product of it.</p>
+						<p>Rich results 4, Site maps 5 & Error documents 2</p>
 					</article>
 					<article>
-						<h4>Javascript</h4>
+						<h3>Javascript</h3>
 						<p>In Javascript I've learned how to manipulate the DOM.</p>
+						<p>JSON 4, DOM 7</p>
 					</article>
 					<article>
-						<h4>PHP</h4>
-						<p></p>
+						<h3>PHP</h3>
+						<p>Sessies 4</p>
 					</article>
 				</div>
 			</section>
