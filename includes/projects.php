1cfa29c88b7f53b84d7bc8c70c0256995969caa7
Aefrir <daanphin@gmail.com>
2025-10-16 11:33:28 +0200

    fix

--- includes/projects.php
+++ includes/projects.php
@@ -10,10 +10,10 @@
         <img src="img/html-project2.png" class="project-images" alt="html image of a table" title="" width="400" height="250">
       </section>
       <section class="bottom-section">
-        <p>I built projects to strengthen my web fundamentals, including an extensive HTML <a href="" class="page-link">reference table</a> and an interactive Game of Thrones image map linking regions to their wiki pages.</p>
+        <p>I built projects to strengthen my web fundamentals, including an extensive HTML reference table and an interactive Game of Thrones image map linking regions to their wiki pages.</p>
         <div class="links">
-          <a href="projects/html/image-map/opdracht-5.html" class="project-link">Image map</a>
-          <a href="projects/html/html-table/index.html" class="project-link">HTML Table</a>
+          <a href="projects/html/image-map/opdracht-5.html" class="project-link" target="_blank">Image map</a>
+          <a href="projects/html/html-table/index.html" class="project-link" target="_blank">HTML Table</a>
         </div>
       </section>
     </article>
@@ -26,9 +26,9 @@
         <img src="img/css-project.png" class="project-images" alt="image of a medical webshop" title="" width="400" height="250">
       </section>
       <section class="bottom-section">
-        <p>I recreated a medical <a href="" class="page-link">webshop</a> design provided in Figma using only HTML and CSS. The result is a 1:1 responsive layout that works seamlessly across desktop and mobile screens, showcasing my attention to detail and front-end styling skills.</p>
+        <p>I recreated a medical webshop design provided in Figma using only HTML and CSS. The result is a 1:1 responsive layout that works seamlessly across desktop and mobile screens, showcasing my attention to detail and front-end styling skills.</p>
         <div class="links">
-          <a href="projects/css/index.html" class="project-link">Webshop</a>
+          <a href="projects/css/index.html" class="project-link" target="_blank">Webshop</a>
         </div>
       </section>
     </article>
@@ -44,7 +44,7 @@
         <p>I created multiple JSON-LD schemas and XML sitemaps for different webpages to improve their search engine visibility. Along the way, I developed a solid understanding of basic SEO principles and how to optimize pages for better ranking.</p>
         <!-- <p>Due to how SEO is done, it is hard for me to provide a good product without showing the underlying code. Hence why I am linking my repository instead.</p> -->
         <div class="links">
-          <a href="#" class="project-link">Repository</a>
+          <a href="#" class="project-link" target="_blank">Repository</a>
         </div>
       </section>
     </article>
@@ -59,8 +59,8 @@
       <section class="bottom-section">
         <p>I learned the basics of JavaScript and how to add interactivity to webpages. The highlight was working with DOM manipulation and JSON data, allowing me to dynamically update and display content on web pages.</p>
         <div class="links">
-          <a href="projects/js/dom/index.html" class="project-link">DOM Webpage</a>
-          <a href="projects/js/json/index.html" class="project-link">JSON Data Webpage</a>
+          <a href="projects/js/dom/index.html" class="project-link" target="_blank">DOM Webpage</a>
+          <a href="projects/js/json/index.html" class="project-link" target="_blank">JSON Data Webpage</a>
         </div>
       </section>
     </article>
@@ -75,7 +75,7 @@
       <section class="bottom-section">
         <p>I'm currently completing my PHP course, where I've learned the fundamentals of backend development. I'm now progressing toward more advanced topics like object-oriented programming (OOP) to build cleaner and more scalable applications.</p>
         <div class="links">
-          <a href="projects/php/index.php" class="project-link">Session Webpage</a>
+          <a href="projects/php/index.php" class="project-link" target="_blank">Session Webpage</a>
         </div>
       </section>
     </article>
